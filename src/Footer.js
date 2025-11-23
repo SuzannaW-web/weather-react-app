6db8a2f749212f9a0e02695eca57a264519f0fe8
SuzannaW-web <suzannawallis@gmail.com>
2025-11-23 23:44:59 +0000

    Added netlify link

--- src/Footer.js
+++ src/Footer.js
@@ -2,9 +2,12 @@ export default function Footer() {
   return (
     <div className="Footer">
       <p>
-        This app was code by Suzanna Wallis and is open-sourced on{" "}
+        This app was coded by Suzanna Wallis and is open-sourced on{" "}
         <a href="https://github.com/SuzannaW-web/weather-react-app">GitHub </a>
-        and hosted on Netlify
+        and{" "}
+        <a href="https://react-weather-by-suzanna.netlify.app/">
+          hosted on Netlify
+        </a>
       </p>
     </div>
   );
