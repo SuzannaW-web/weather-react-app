352a681c58110409303ab44c5be91eb0d3cf0e47
SuzannaW-web <suzannawallis@gmail.com>
2025-12-04 22:35:58 +0000

    tidied units spacing

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -18,6 +18,7 @@ export default function WeatherTemperature(props) {
       <div className="WeatherTemperature">
         <span className="temperature">{Math.round(props.celsius)}</span>
         <span className="unit">
+          {" "}
           °C |{" "}
           <a href="/" onClick={showFahrenheit}>
             °F
@@ -29,9 +30,8 @@ export default function WeatherTemperature(props) {
     let fahrenheit = (props.celsius * 9) / 5 + 32;
     return (
       <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit)}</span>
+        <span className="temperature">{Math.round(fahrenheit)} </span>
         <span className="unit">
-          {" "}
           <a href="/" onClick={showCelsius}>
             °C
           </a>{" "}
