47bc23a7f6eef78c4f5445f0419c96464304f87f
SuzannaW-web <suzannawallis@gmail.com>
2025-12-04 22:00:52 +0000

    improved styling

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,31 +1,45 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <div class="row">
-        <div class="col-5  ">
-          <FormattedDate date={props.data.date} />
+      {/* Header */}
+      <div className="text-center weather-header">
+        <FormattedDate date={props.data.date} />
+        <h1>{props.data.city}</h1>
+      </div>
 
-          <h1>{props.data.city}</h1>
-          <ul>
-            <li className="text-capitalize">{props.data.description}</li>
-            <li className="col-6 ">Humidity: {props.data.humidity}%</li>
-            <li className="col-6 ">Wind: {props.data.wind} km/h</li>
-          </ul>
+      {/* Three columns */}
+      <div className="row weather-cards">
+        {/* Card 1: details */}
+        <div className="col-12 col-md-4">
+          <div className="weather-card details-card">
+            <ul>
+              <li className="text-capitalize">{props.data.description}</li>
+              <li>Humidity: {props.data.humidity}%</li>
+              <li>Wind: {props.data.wind} km/h</li>
+            </ul>
+          </div>
         </div>
 
-        <div class="col-4">
-          <WeatherTemperature celsius={props.data.temperature} />
+        {/* Card 2: temperature */}
+        <div className="col-12 col-md-4">
+          <div className="weather-card temp-card text-center">
+            <WeatherTemperature celsius={props.data.temperature} />
+          </div>
         </div>
 
-        <div class="col-3  ">
-          <img
-            src={props.data.icon}
-            alt={props.data.description}
-            className="weather-icon"
-          />
+        {/* Card 3: icon */}
+        <div className="col-12 col-md-4">
+          <div className="weather-card icon-card text-center">
+            <img
+              src={props.data.icon}
+              alt={props.data.description}
+              className="weather-icon"
+            />
+          </div>
         </div>
       </div>
     </div>
