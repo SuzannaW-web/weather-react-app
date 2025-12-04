e9cc1c8b77e365bec120de17f4d511330e8e3a78
SuzannaW-web <suzannawallis@gmail.com>
2025-12-04 18:31:35 +0000

    Create component for units

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function FormattedDate(props) {
-  let days = [
+  const days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -10,18 +10,35 @@ export default function FormattedDate(props) {
     "Friday",
     "Saturday",
   ];
-  let day = days[props.date.getDay()];
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const day = days[props.date.getDay()];
+  const date = props.date.getDate();
+  const month = months[props.date.getMonth()];
+
   let hours = props.date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
+  if (hours < 10) hours = `0${hours}`;
+
   let minutes = props.date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+  if (minutes < 10) minutes = `0${minutes}`;
+
   return (
     <div>
-      {day} {hours}:{minutes}
+      {day} {date} {month}, {hours}:{minutes}
     </div>
   );
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,36 +1,32 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <FormattedDate date={props.data.date} />
-
-      <h1>{props.data.city}</h1>
-
       <div class="row">
-        <div class="col-auto mx-auto  ">
-          <div className="row align-items-center text-center">
-            <div className="col-auto">
-              {" "}
-              <span className="temperature">
-                {Math.round(props.data.temperature)}
-              </span>
-              <span className="unit">°C</span>
-            </div>
-            <div className="col-auto ">
-              <img
-                src={props.data.icon}
-                alt={props.data.description}
-                className="weather-icon"
-              />
-              <li className="text-capitalize">{props.data.description}</li>
-            </div>
-          </div>
+        <div class="col-5  ">
+          <FormattedDate date={props.data.date} />
+
+          <h1>{props.data.city}</h1>
+          <ul>
+            <li className="text-capitalize">{props.data.description}</li>
+            <li className="col-6 ">Humidity: {props.data.humidity}%</li>
+            <li className="col-6 ">Wind: {props.data.wind} km/h</li>
+          </ul>
         </div>
-      </div>
-      <div className="row conditions">
-        <div className="col-6 ">Humidity: {props.data.humidity}%</div>
-        <div className="col-6 ">Wind: {props.data.wind} km/h</div>
+
+        <div class="col-4">
+          <WeatherTemperature celsius={props.data.temperature} />
+        </div>
+
+        <div class="col-3  ">
+          <img
+            src={props.data.icon}
+            alt={props.data.description}
+            className="weather-icon"
+          />
+        </div>
       </div>
     </div>
   );
